d82b8176a71e920883e1c72b2e4b01ae41356c7f
jango_blockchained <jan.goischke@gmail.com>
2022-10-19 20:48:16 +0000

    autocommit

---
+++ xtender/xtender/doctype/repack_request/repack_request.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, jango_blockchained and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Repack Request', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ xtender/xtender/doctype/repack_request/test_repack_request.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022, jango_blockchained and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestRepackRequest(unittest.TestCase):
+	pass
